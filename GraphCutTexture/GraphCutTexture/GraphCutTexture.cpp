5ced35700e1ad23992bf61984e56566894cc9ecf
Takashi Ijiri <takashi.ijiri80@gmail.com>
2016-06-21 20:08:48 +0900

    consider old seams

--- GraphCutTexture/GraphCutTexture/GraphCutTexture.cpp
+++ GraphCutTexture/GraphCutTexture/GraphCutTexture.cpp
@@ -30,26 +30,15 @@ public:
 	int W,H;
 	byte *rgba;
 	
-	TImage(){
-		W = H = 0;
-		rgba = 0;
+	TImage(){ W = H = 0; rgba = 0; }
+	~TImage(){ 
+		if( rgba ) delete[] rgba;
 	}
-
-	bool isPixBlue(const int idx) const
-	{
-		return rgba[idx + 0] == 0 && 
-			   rgba[idx + 1] == 0 && 
-			   rgba[idx + 2] == 255;
-	}
-
-
-
 	void allocate(const int _W, const int _H){
 		W = _W;
 		H = _H;
 		rgba = new byte[W * H * 4];
 	}
-
 
 	bool load(const char *fname )
 	{
@@ -126,6 +115,15 @@ public:
 
 
 
+static void t_exportBmp(const char *fname, const int W, const int H,  const byte *rgba)
+{
+	TImage test;
+	test.allocate(W, H);
+	memcpy(test.rgba, rgba, 4 * sizeof(byte) * W * H);
+	test.saveAsFile(fname, 0);
+}
+
+
 static inline bool t_isBlue(const int I, const byte* rgba)
 {
 	return rgba[I + 0] == 0 && rgba[I + 1] == 0 && rgba[I + 2] == 255;
@@ -138,46 +136,28 @@ class PixInf
 {
 public:
 	//patch id
-	int  patchID;
-
-	//edge cost (if exist)
-	double eCostX;
-	double eCostY;
-
+	int    patchID;
+	double eCostX ;
+	double eCostY ;
 
 	PixInf(){
 		patchID = -1;
 		eCostX = eCostY = 0;
 	}
-
-	void setPatchId(const int _patchId){
-		patchID =_patchId;
-	}
-
 };
+
 
 static inline double t_calcEdgeVal(
 	const byte *As, const byte *Bs,
 	const byte *At, const byte *Bt )
 {
-	double ds = 
-		(As[0] - Bs[0]) * (As[0] - Bs[0]) + 
-		(As[1] - Bs[1]) * (As[1] - Bs[1]) + 
-		(As[2] - Bs[2]) * (As[2] - Bs[2]);
-	double dt =
-		(At[0] - Bt[0]) * (At[0] - Bt[0]) +
-		(At[1] - Bt[1]) * (At[1] - Bt[1]) +
-		(At[2] - Bt[2]) * (At[2] - Bt[2]);
-
+	double ds = (As[0] - Bs[0]) * (As[0] - Bs[0]) + 
+				(As[1] - Bs[1]) * (As[1] - Bs[1]) + 
+				(As[2] - Bs[2]) * (As[2] - Bs[2]);
+	double dt = (At[0] - Bt[0]) * (At[0] - Bt[0]) +
+				(At[1] - Bt[1]) * (At[1] - Bt[1]) +
+				(At[2] - Bt[2]) * (At[2] - Bt[2]);
 	return sqrt(ds) + sqrt(dt);
-}
-
-static void t_exportBmp(const char *fname, const int W, const int H,  const byte *rgba)
-{
-	TImage test;
-	test.allocate(W, H);
-	memcpy(test.rgba, rgba, 4 * sizeof(byte) * W * H);
-	test.saveAsFile(fname, 0);
 }
 
 
@@ -214,8 +194,8 @@ eCostY represent (b) edge cost
 
 static void t_graphCutTexture_placePatch
 (
-	const int W, 
-	const int H,
+	const int   W, 
+	const int   H,
 	const byte *imgA  , //rgba base image
 	const byte *imgB  , //rgba new patch
 	byte       *resImg, //rgba results
@@ -223,71 +203,55 @@ static void t_graphCutTexture_placePatch
 	const int newPatchId
 )
 {
-	//count pixel nodes (overlap pixel) and edge nodes
-
-	byte *imgFlg       = new byte[W*H]; //0 non, 1:imgA, 2:imgB, 3:both
-	int  *imgNodeID    = new int [W*H];
-	int  *mapNodeToPix = new int [W*H];
+	const int WH = W*H;
 
 	int N_pNode = 0, N_eNode = 0;
+	byte *imgFlg       = new byte[WH]; //0 non, 1:imgA, 2:imgB, 3:both
+	int  *imgNodeID    = new int [WH];
 
-	for (int y = 0; y < H; ++y)
+	//count pixel nodes (overlap pixels) 
+	for( int i = 0; i < WH; ++i)
 	{
-		for (int x = 0; x < W; ++x)
-		{
-			const int I = x + y*W;
-			bool isA = !t_isBlue(4 * I, imgA);
-			bool isB = !t_isBlue(4 * I, imgB);
-			imgFlg[ I ] = ( !isA && !isB ) ? 0 :
-						  (  isA && !isB ) ? 1 :
-						  ( !isA &&  isB ) ? 2 : 3;
+		bool isA = !t_isBlue(4 * i, imgA);
+		bool isB = !t_isBlue(4 * i, imgB);
+		imgFlg[ i ] = ( !isA && !isB ) ? 0 :
+					  (  isA && !isB ) ? 1 :
+					  ( !isA &&  isB ) ? 2 : 3;
 
-			imgNodeID[I] = -1;
-
-			if ( imgFlg[I] == 3){
-				imgNodeID[I] = N_pNode;
-				mapNodeToPix[N_pNode] = I;
-				N_pNode++;
-			}
-		}
+		imgNodeID[i] = -1;
+		if ( imgFlg[i] == 3) imgNodeID[i] = N_pNode++;
 	}
 
-	for (int y = 0; y < H; ++y)
+	//count pixel nodes (existing edges)
+	for( int i = 0; i < WH; ++i)
 	{
-		for (int x = 0; x < W; ++x)
-		{
-			const int I = x + y*W;
-			if (x != W - 1 && pixInfo[I].eCostX > 0 && imgNodeID[I] != -1 && imgNodeID[ I + 1 ] != -1) N_eNode++;
-			if (y != H - 1 && pixInfo[I].eCostY > 0 && imgNodeID[I] != -1 && imgNodeID[ I + W ] != -1) N_eNode++;
-		}
+		if (pixInfo[i].eCostX > 0 && imgFlg[i] == 3 && imgFlg[ i + 1 ] == 3) N_eNode++;
+		if (pixInfo[i].eCostY > 0 && imgFlg[i] == 3 && imgFlg[ i + W ] == 3) N_eNode++;
 	}
 
 
 	//generate a graph
-	const int SourceID = N_eNode + N_pNode   ;
-	const int SinkID   = N_eNode + N_pNode +1;
+	const int SourceID = N_pNode + N_eNode   ;
+	const int SinkID   = N_pNode + N_eNode +1;
 	TFlowNetwork_BK4 graph( N_pNode + N_eNode + 2, N_pNode * 5 + N_eNode * 2, SourceID, SinkID);
 
 	int seamNodeI = 0;
 
-
-	//gen n-Link
-	for (int y = 0; y < H; ++y) for (int x = 0; x < W; ++x)
+	//gen n-Link (img[S]-img[T])
+	for (int S = 0; S < WH; ++S) if( imgNodeID[ S ] >= 0)
 	{
-		const int S = x + y*W;
-
-		if (imgNodeID[ S ] == -1) continue;
+		const int x = S % W;
+		const int y = S / W;
 
 		for( int k = 0; k < 2; ++k)
 		{
-			const int T = (k == 0) ? S + 1 : S + W;
+			if ( (k == 0 && x == W - 1) || (k == 1 && y == H - 1) ) continue;
 
-			if (k == 0 && x == W - 1) continue;
-			if (k == 1 && y == H - 1) continue;
+			const int T = (k == 0) ? S + 1 : S + W;
 			if (imgNodeID[T] == -1  ) continue;
 
 			const double eCost = ( k == 0 ) ? pixInfo[S].eCostX : pixInfo[S].eCostY;
-			const double capa  = t_calcEdgeVal( &imgA[4*S], &imgB[4 * S], &imgA[4*T], &imgB[4*T]);
+			const double capa  = t_calcEdgeVal( &imgA[4*S], &imgB[4*S], &imgA[4*T], &imgB[4*T]);
 
 			if ( eCost > 0 )
 			{
@@ -303,62 +267,78 @@ static void t_graphCutTexture_placePatch
 	}
 
 	//gen t Link (strict constrains)
-	int fixAn = 0, fixBn = 0;
-	for (int y = 0; y < H; ++y) for (int x = 0; x < W; ++x)
+	for (int y = 0; y < H; ++y)
 	{
-		const int I = x + y*W;
-		if ( imgNodeID[I] == -1 ) continue;
+		for (int x = 0; x < W; ++x)
+		{
+			const int I = x + y*W;
+			if ( imgNodeID[I] == -1 ) continue;
 
-		bool bAdjA = (x > 0    && imgFlg[I - 1] == 1) || (y > 0    && imgFlg[I - W] == 1) || 
-			         (x <W - 1 && imgFlg[I + 1] == 1) || (y <H - 1 && imgFlg[I + W] == 1);
-		bool bAdjB = (x > 0    && imgFlg[I - 1] == 2) || (y > 0    && imgFlg[I - W] == 2) ||
-			         (x <W - 1 && imgFlg[I + 1] == 2) || (y <H - 1 && imgFlg[I + W] == 2);
+			bool bAdjA = (x > 0 && imgFlg[I-1] == 1) || (y > 0 && imgFlg[I-W] == 1) || (x < W-1 && imgFlg[I+1] == 1) || (y < H-1 && imgFlg[I+W] == 1);
+			bool bAdjB = (x > 0 && imgFlg[I-1] == 2) || (y > 0 && imgFlg[I-W] == 2) || (x < W-1 && imgFlg[I+1] == 2) || (y < H-1 && imgFlg[I+W] == 2);
 		
-		if ( bAdjA && !bAdjB) graph.add_tLink(SourceID, SinkID, imgNodeID[I], 100000, 0);
-		if (!bAdjA &&  bAdjB) graph.add_tLink(SourceID, SinkID, imgNodeID[I], 0, 100000);
+			if ( bAdjA && !bAdjB) graph.add_tLink(SourceID, SinkID, imgNodeID[I], 100000, 0);
+			if (!bAdjA &&  bAdjB) graph.add_tLink(SourceID, SinkID, imgNodeID[I], 0, 100000);
+		}
 	}
-
 
 	//graph cut
 	byte *minCut = new byte[N_pNode + N_eNode + 2];
 	graph.calcMaxFlow(SourceID, SinkID, minCut);
 
-	//update edge info
-	for (int y = 0; y < H; ++y) for (int x = 0; x < W; ++x)
+	//add new edge
+	for (int y = 0; y < H; ++y)
 	{
-		const int I = x + y*W, I4 = I * 4;
-		if (imgNodeID[I] == -1) continue;
-
-		//imgBに飲み込まれてたら削除
-		//imgAとBの境界にあったら新たに追加
-	}
-
-	//update image
-	for (int y = 0; y < H; ++y) for (int x = 0; x < W; ++x)
-	{
-		const int I = x + y*W, I4 = I*4;
-		
-		resImg[I4  ] = resImg[I4+1] = 0;
-		resImg[I4+2] = 255;
-
-		if      (imgFlg[I] == 1) memcpy(&resImg[I4], &imgA[I4], sizeof(byte) * 4);
-		else if (imgFlg[I] == 2) memcpy(&resImg[I4], &imgB[I4], sizeof(byte) * 4);
-		else if (imgFlg[I] == 3)
-		{		
-			if( minCut[imgNodeID[I]] ) memcpy(&resImg[I4], &imgA[I4], sizeof(byte) * 4);
-			else                       memcpy(&resImg[I4], &imgB[I4], sizeof(byte) * 4);
+		for (int x = 0; x < W; ++x)
+		{
+			const int S  = x + y * W, Tx = S + 1, Ty = S + W;
+			if (x < W - 1 && imgFlg[S] == 3 && imgFlg[Tx] == 3 && minCut[imgNodeID[S]] != minCut[imgNodeID[Tx]])
+			{
+				pixInfo[S].eCostX = t_calcEdgeVal( &imgA[4*S], &imgB[4*S], &imgA[4*Tx], &imgB[4*Tx]);
+			}
+			if (y < H - 1 && imgFlg[S] == 3 && imgFlg[Ty] == 3 && minCut[imgNodeID[S]] != minCut[imgNodeID[Ty]])
+			{
+				pixInfo[S].eCostY = t_calcEdgeVal( &imgA[4*S], &imgB[4*S], &imgA[4*Ty], &imgB[4*Ty]);
+			}
 		}
 	}
 
+	//update image
+	byte blue[] = {0,0,255,0};
+	for (int i = 0; i < WH; ++i) memcpy( &resImg[i*4], blue, sizeof(byte)*4 );
+
+	for (int i = 0; i < WH; ++i) if( imgFlg[i] != 0 )
+	{
+		const int I4 = i * 4;
+		if ( imgFlg[i] == 1 || (imgFlg[i] == 3 && minCut[imgNodeID[i]] ))
+		{
+			memcpy(&resImg[I4], &imgA[I4], sizeof(byte) * 4);
+		}
+		else if (imgFlg[i] == 2 || (imgFlg[i] == 3 && !minCut[imgNodeID[i]] )) 
+		{
+			memcpy(&resImg[I4], &imgB[I4], sizeof(byte) * 4);
+			pixInfo[i].patchID = newPatchId;
+		}
+	}
+
+	//remove unnecessary edge
+	for (int y = 0; y < H; ++y)
+	{
+		for (int x = 0; x < W; ++x)
+		{
+			const int S  = x + y * W, Tx = S + 1, Ty = S + W;
+			if( x < W - 1 && pixInfo[S].patchID == pixInfo[Tx].patchID ) pixInfo[S].eCostX = 0;
+			if( y < H - 1 && pixInfo[S].patchID == pixInfo[Ty].patchID ) pixInfo[S].eCostY = 0;
+		}
+	}
 
 	delete[] imgFlg;
 	delete[] imgNodeID;
-	delete[] mapNodeToPix;
 	delete[] minCut;
 }
 
 
-
+static byte Col[5][4] = { {64,64,64,0}, {0,0,255,0}, {0,255,128,0}, {255,0,0,0}, {255,255,0,0} };
 
 
 
@@ -371,24 +351,20 @@ static void t_graphCutTexture(const vector<TImage> &imgs, TImage &outImg)
 	byte   *resImg  = new byte  [W*H * 4];
 
 
-
-	//一枚目を配置し
+	//一枚目 配置
 	memcpy(resImg, imgs[0].rgba, sizeof(byte) * W * H * 4);
 
-	for( int i=0; i < W*H; ++i)
-	{
-		if( !t_isBlue(4 * i, resImg) ) pixInfo[i].setPatchId( 0 );
-	}
+	for( int i=0; i < W*H; ++i) pixInfo[i].patchID = t_isBlue(4 * i, resImg) ? -1 : 0; 
 
 
 	//2枚目以降を追加
 	for( int i = 1; i < imgs.size(); ++i)
 	{
-		printf("counter %d\n", i);
-
 		byte *tmp = new byte[W*H * 4];
 		t_graphCutTexture_placePatch ( W,H, resImg, imgs[i].rgba, tmp, pixInfo, i );
 		memcpy( resImg, tmp, sizeof(byte) * W * H * 4);
+
+		fprintf( stderr, "add patch\n");
 
 		CString str;
 		str.Format("test%d.bmp", i);
@@ -396,7 +372,22 @@ static void t_graphCutTexture(const vector<TImage> &imgs, TImage &outImg)
 
 		delete[] tmp;
 
+		//export
+		byte *patchId = new byte[ W * H * 4 ];
+		byte *seamImg = new byte[ W * H * 4 ];
+		for (int i = 0; i < W*H; ++i)
+		{
+			int ci = (pixInfo[i].patchID + 1) % 5;
+			memcpy( &patchId[4*i], Col[ci], sizeof(byte) * 4);
+			seamImg[4*i + 0] = pixInfo[i].eCostX > 0 || pixInfo[i].eCostY > 0 ? 255 : 0;
+		}
+		t_exportBmp(str + "patch.bmp", W,H, patchId);
+		t_exportBmp(str + "seam.bmp" , W,H, seamImg);
+		delete[] patchId;
+		delete[] seamImg;
 	}
+
+
 
 	delete[] pixInfo;
 	delete[] resImg ;
@@ -436,11 +427,11 @@ int main()
 
 
 	//input image
-	vector<TImage> imgs(3);
+	vector<TImage> imgs(4);
 	imgs[0].load("p1.bmp");
 	imgs[1].load("p2.bmp");
 	imgs[2].load("p3.bmp");
-	//imgs[3].load("p4.bmp");
+	imgs[3].load("p4.bmp");
 
 	//resolutions of images are suppose to be the same
 	const int W = imgs[0].W;
@@ -451,12 +442,6 @@ int main()
 	outImg.allocate(W, H);
 
 	t_graphCutTexture(imgs, outImg);
-
-
-
-
-
-
 
     return nRetCode;
 }
